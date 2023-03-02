c156567f712c8b1bae33aaaa9f74257bedd5706f
iamapusaha <apusaha392@gmail.com>
2023-03-02 12:05:23 +0600

    display data on UI

--- js/index.js
+++ js/index.js
@@ -3,9 +3,28 @@ const loadPhones = async () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data.data)
+        displayPhones(data.data)
     } catch (error) {
         console.log(error);
     }
+}
+const displayPhones = (phones) => {
+    const phoneContainer = document.getElementById('card-container')
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneCard = document.createElement('div')
+        phoneCard.classList.add('col')
+        phoneCard.innerHTML = `
+    <div class="card p-2">
+                        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                                to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+    `
+        phoneContainer.appendChild(phoneCard)
+    });
 }
 loadPhones();
