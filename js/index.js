c64741e301a67cf068254f6102fb119808b2aa71
iamapusaha <apusaha392@gmail.com>
2023-03-02 17:45:58 +0600

    added search option

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-const loadPhones = async () => {
+const loadPhones = async (search) => {
     try {
-        const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+        const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
         const res = await fetch(url);
         const data = await res.json();
         displayPhones(data.data)
@@ -9,14 +9,15 @@ const loadPhones = async () => {
     }
 }
 const displayPhones = (phones) => {
-    const phoneContainer = document.getElementById('card-container')
+    const phoneContainer = document.getElementById('card-container');
+    phoneContainer.textContent = '';
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div')
         phoneCard.classList.add('col')
         phoneCard.innerHTML = `
     <div class="card p-2">
-                        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+                        <img src="${phone.image}" class="card-img-top img-fluid p-4" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
@@ -27,4 +28,10 @@ const displayPhones = (phones) => {
         phoneContainer.appendChild(phoneCard)
     });
 }
-loadPhones();
+document.getElementById('btn-search').addEventListener('click', function () {
+    const inputField = document.getElementById('input-field');
+
+    const inputValue = inputField.value;
+    inputField.value = '';
+    loadPhones(inputValue);
+})
